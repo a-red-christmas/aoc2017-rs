28adc6b977db7665f156da30277e61235082f3d8
Patrick Barrett <patrick@psbarrett.com>
2017-12-09 09:33:54 -0600

    day 4, part 2 (azdle)

--- day4/src/azdle.rs
+++ day4/src/azdle.rs
@@ -1,19 +1,23 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 
 fn main() {
     use std::io;
     use std::io::prelude::*;
 
     let mut validator = Validator::new();
+    let mut anag_validator = AnagramValidator::new();
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let passphrase = line.unwrap();
-        println!("{}: {} ({}/{})",
+        println!("{}: {} ({}/{}) | {} ({}/{})",
             passphrase,
             validator.validate(&passphrase),
             validator.counts().0,
-            validator.counts().1);
+            validator.counts().1,
+            anag_validator.validate(&passphrase),
+            anag_validator.counts().0,
+            anag_validator.counts().1);
     }
 }
 
@@ -52,6 +56,47 @@ impl Validator {
     }
 }
 
+struct AnagramValidator {
+    set: BTreeSet<BTreeMap<char, u64>>,
+    count_tested: u64,
+    count_valid: u64,
+}
+
+impl AnagramValidator {
+    fn new() -> Self {
+        Self {
+            set: BTreeSet::new(),
+            count_tested: 0,
+            count_valid: 0,
+        }
+    }
+
+    fn validate(&mut self, passphrase: &str) -> bool {
+        self.set.clear();
+        self.count_tested += 1;
+
+        for word in passphrase.split(" ") {
+            let mut counts = BTreeMap::new();
+
+            for c in word.chars() {
+                *counts.entry(c).or_insert(0) += 1;
+            }
+
+            match self.set.insert(counts) {
+                true => (),
+                false => return false,
+            }
+        }
+
+        self.count_valid += 1;
+        true
+    }
+
+    fn counts(&self) -> (u64, u64) {
+        (self.count_valid, self.count_tested)
+    }
+}
+
 #[test]
 fn valid_basic() {
     let val = "aa bb cc dd ee"; // is valid.
@@ -68,7 +113,6 @@ fn invalid_basic() {
     let mut validator = Validator::new();
     assert_eq!(validator.validate(val), false);
     assert_eq!(validator.counts(), (0, 1));
-
 }
 
 #[test]
@@ -78,6 +122,54 @@ fn valid_len_diff() {
     let mut validator = Validator::new();
     assert_eq!(validator.validate(val), true);
     assert_eq!(validator.counts(), (1, 1));
+}
 
+
+#[test]
+fn valid_by_anag() {
+    let val = "abcde fghiji"; // is a valid passphrase.
+
+    let mut validator = AnagramValidator::new();
+    assert_eq!(validator.validate(val), true);
+    assert_eq!(validator.counts(), (1, 1));
+}
+
+#[test]
+fn invalid_by_anag() {
+    let val = "abcde xyz ecdab"; // is not valid
+    //the letters from the third word can be rearranged to form the first word.
+
+    let mut validator = AnagramValidator::new();
+    assert_eq!(validator.validate(val), false);
+    assert_eq!(validator.counts(), (0, 1));
+}
+
+#[test]
+fn valid_by_anag_only_partial() {
+    let val = "a ab abc abd abf abj"; // is a valid passphrase,
+    // because all letters need to be used when forming another word.
+
+    let mut validator = AnagramValidator::new();
+    assert_eq!(validator.validate(val), true);
+    assert_eq!(validator.counts(), (1, 1));
+}
+
+#[test]
+fn valid_by_anag_letter_counts() {
+    let val = "iiii oiii ooii oooi oooo"; // is valid.
+
+    let mut validator = AnagramValidator::new();
+    assert_eq!(validator.validate(val), true);
+    assert_eq!(validator.counts(), (1, 1));
+}
+
+#[test]
+fn invalid_by_anag_all_same_count() {
+    let val = "oiii ioii iioi iiio"; // is not valid
+        //any of these words can be rearranged to form any other word.
+
+    let mut validator = AnagramValidator::new();
+    assert_eq!(validator.validate(val), false);
+    assert_eq!(validator.counts(), (0, 1));
 }
 
